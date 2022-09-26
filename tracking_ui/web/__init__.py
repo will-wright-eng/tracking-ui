bb24ffc6adcfa15025e6d22d06906d0aaeb8b40e
william-cass-wright <william.cass.wright@gmail.com>
2022-09-26 00:00:32 -0700

    shift over to s3rius cookiecutter template

---
+++ tracking_ui/web/__init__.py
@@ -0,0 +1,1 @@
+"""WEB API for tracking_ui."""
---
+++ tracking_ui/web/application.py
@@ -0,0 +1,35 @@
+from importlib import metadata
+
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+
+from tracking_ui.web.api.router import api_router
+from tracking_ui.web.lifetime import register_shutdown_event, register_startup_event
+
+
+def get_app() -> FastAPI:
+    """
+    Get FastAPI application.
+
+    This is the main constructor of an application.
+
+    :return: application.
+    """
+    app = FastAPI(
+        title="tracking_ui",
+        description="this is a tracking us",
+        version=metadata.version("tracking_ui"),
+        docs_url="/api/docs",
+        redoc_url="/api/redoc",
+        openapi_url="/api/openapi.json",
+        default_response_class=UJSONResponse,
+    )
+
+    # Adds startup and shutdown events.
+    register_startup_event(app)
+    register_shutdown_event(app)
+
+    # Main router for the API.
+    app.include_router(router=api_router, prefix="/api")
+
+    return app
