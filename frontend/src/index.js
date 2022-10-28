974135c858de809a5dad9b3af3b9ffe357093c21
william-cass-wright <william.cass.wright@gmail.com>
2022-10-28 04:11:57 -0700

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,17 @@
-import React from "react";
-// import { render } from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import { ChakraProvider } from "@chakra-ui/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import Header from "./components/Header";
-import Todos from "./components/Todos";  // new
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-function App() {
-  return (
-    <ChakraProvider>
-      <Header />
-      <Todos />  {/* new */}
-    </ChakraProvider>
-  )
-}
-
-// const rootElement = document.getElementById("root")
-// render(<App />, rootElement)
-
-
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
